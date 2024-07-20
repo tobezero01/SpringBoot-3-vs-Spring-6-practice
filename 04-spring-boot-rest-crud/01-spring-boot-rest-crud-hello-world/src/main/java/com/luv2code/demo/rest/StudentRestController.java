49b3c63cf6fb496ec769407f1c2e9720d4557308
tobezero01 <ducnhuad@gmail.com>
2024-07-20 15:06:41 +0700

    path variable

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> listStudents;
+
+    @PostConstruct
+    public void loadData() {
+        listStudents = new ArrayList<>();
+        listStudents.add(new Student("NHU" , "DUC"));
+        listStudents.add(new Student("NHU DInh" , "DUC"));
+        listStudents.add(new Student("NHU DINH" , "TRI"));
+    }
+
     @GetMapping("/listStudents")
     public List<Student> getStudents() {
-        List<Student> list = new ArrayList<>();
-        list.add(new Student("NHU" , "DUC"));
-        list.add(new Student("NHU DInh" , "DUC"));
-        list.add(new Student("NHU DINH" , "TRI"));
-        return list;
+        return listStudents;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+
+        if (studentId <= 0 || studentId > listStudents.size()) {
+            //throw new
+        }
+        return listStudents.get(studentId);
     }
 }
