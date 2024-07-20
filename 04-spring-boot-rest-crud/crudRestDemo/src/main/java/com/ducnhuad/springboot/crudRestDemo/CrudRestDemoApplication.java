897da5ca7b039e2c1844c6ce3f24d33b0c67dad4
tobezero01 <ducnhuad@gmail.com>
2024-07-20 16:16:18 +0700

    ADD PROJECT CRUD DEMO

---
+++ 04-spring-boot-rest-crud/crudRestDemo/src/main/java/com/ducnhuad/springboot/crudRestDemo/CrudRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ducnhuad.springboot.crudRestDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestDemoApplication.class, args);
+	}
+
+}
