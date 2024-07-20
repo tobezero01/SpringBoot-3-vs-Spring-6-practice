a76328680270dacf9f636b5a30750cc6d6193f32
tobezero01 <ducnhuad@gmail.com>
2024-07-20 15:55:22 +0700

    global exceptionhandle

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -39,33 +39,5 @@ public class StudentRestController {
 
 
     // Add an exception handler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        // create a Student error response
-        StudentErrorResponse error = new StudentErrorResponse();
 
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return response Entity
-
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return response Entity
-
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-    }
 }
