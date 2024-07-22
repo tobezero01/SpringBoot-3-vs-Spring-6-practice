c2948fb3582b6b35929b94d2353c313735e8c440
tobezero01 <ducnhuad@gmail.com>
2024-07-22 21:33:56 +0700

    thymeleaf controller data to view

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/helloController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/helloController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +18,26 @@ public class helloController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloWorld"; // return view
+    }
+
+
+    // need a controller method to read form data and
+    // add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request , Model model) {
+
+        // read the request parameter from html
+        String name = request.getParameter("studentName");
+
+        //convert the data to all caps
+        name = name.toUpperCase();
+
+        // create message
+        String res = "Hello " + name;
+
+        model.addAttribute("message" , res);
+
+        return "helloWorld";
     }
 }
