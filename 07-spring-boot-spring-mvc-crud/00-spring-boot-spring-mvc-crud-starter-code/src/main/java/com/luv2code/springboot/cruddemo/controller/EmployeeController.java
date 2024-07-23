af3e3b521a76e3bc86d4536e8c4376521b414bdc
tobezero01 <ducnhuad@gmail.com>
2024-07-23 21:56:01 +0700

    mvc Crud api redirect

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for /list
+    @GetMapping("/list")
+    public String listEmp(Model model) {
+        List<Employee> employees = employeeService.findAll();
+
+        model.addAttribute("employees", employees);
+
+        return "list-employee";
+    }
+}
