f31f013a5fd95b6adb472a1fa032825940c91060
tobezero01 <ducnhuad@gmail.com>
2024-07-23 18:10:12 +0700

    validation  invalid number freePasses

--- 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/springdemo/mvc/validationDemo/Customer.java
+++ 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/springdemo/mvc/validationDemo/Customer.java
@@ -10,10 +10,11 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName ="";
 
+    @NotNull(message = "is required")
     @Min(value = 0 , message = " freePasses >= 0")
     @Max(value = 10, message = " freePasses <= 10")
-    private int freePasses;
-
+    private Integer freePasses;
+    @NotNull(message = "is required")
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 
@@ -27,11 +28,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
