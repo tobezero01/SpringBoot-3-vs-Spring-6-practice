cbb44a66a6fceeaddc1b6159c568949ad04776e5
tobezero01 <ducnhuad@gmail.com>
2024-07-23 17:50:50 +0700

    validation  max and min range

--- 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/springdemo/mvc/validationDemo/Customer.java
+++ 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/springdemo/mvc/validationDemo/Customer.java
@@ -1,10 +1,11 @@
 package com.springdemo.mvc.validationDemo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
-
 
     private String firstName;
 
@@ -12,11 +13,24 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName ="";
 
+    @Min(value = 0 , message = " freePasses >= 0")
+    @Max(value = 10, message = " freePasses <= 10")
+    private int freePasses;
+
     public Customer(){}
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, int freePasses) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.freePasses = freePasses;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 
     public String getFirstName() {
@@ -38,9 +52,10 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+        return "Customer : " +
+                "firstName='" + firstName + '\'' + "\n" +
+                ", lastName='" + lastName + '\'' +"\n" +
+                ", freePasses=" + freePasses
+                ;
     }
 }
